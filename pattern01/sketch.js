deeee3eafc5827b95fe9a12bd81275e30b22596f
andou yuuma <yuuma2021cr@nzu.jp>
2022-04-20 08:32:13 +0900

    for文に苦戦中

--- pattern01/sketch.js
+++ pattern01/sketch.js
@@ -7,17 +7,26 @@ window.addEventListener('load', function(){
     // キャンバスの大きさを指定する。
     view.viewSize = new Size(500, 500);
 
+    project.currentStyle = {
+        strokeColor: '#000000', // 線の色
+        fillColor: null, // 塗りなし
+        strokeWidth: 1, // 線の幅
+    };
+
     let backgroundColor = new Color('#ebe546');
     let lineColor1 = new Color('#f01313');
     let lineColor2 = new Color('#000000');
-
+        
     // 長方形 : 左上の座標, 幅と高さ
-    let background = Path.Rectangle([0, 0], [500, 500]);
+    let background = Path.Rectangle([0, 0], view.viewSize);
     background.fillColor = backgroundColor;
 
-    // 直線 : 開始点の座標, 終了点の座標
-    let line1 = Path.Line([0, 1], [500, 1]);
-    line1.fillColor = lineColor1;
+    for (let i = 1; i < 500; i = i + 50){
+        let lineA = Path.Line([0, i], [500, i]);
+        let lineB = Path.Line([i, 0], [i, 500]);
+    }
+    lineA.fillColor = lineColor1;
+
 
     // 画面を描く。
     view.draw();
