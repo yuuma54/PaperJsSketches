bdcdb32e1a745b9fe81d2fb8fbd55a925b1c3a91
andou yuuma <yuuma2021cr@nzu.jp>
2022-05-17 14:23:25 +0900

    15の変更

--- sketch15/sketch.js
+++ sketch15/sketch.js
@@ -5,7 +5,7 @@ let sketchName = 'My Sketch';
 // P5.jsやProcessingのsetupと同じ役割。
 window.addEventListener('load', function(){
     // キャンバスの大きさを指定する。
-    view.viewSize = new Size(500, 500);
+    view.viewSize = new Size(500, 700);
 
     function pickRandom(array) {
         let i = Math.floor(Math.random() * array.length);
@@ -13,12 +13,13 @@ window.addEventListener('load', function(){
     }
 
     project.currentStyle = {
-        strokeColor: '#A44A3F', // 線の色
+        strokeColor: '#000000', // 線の色
         fillColor: null, // 塗りの色
-        strokeWidth: 0 // 線の幅
+        strokeWidth: 1 // 線の幅
     };
 
-    let backgroundColor = new Color('#fff1f0');
+    //let backgroundColor = new Color('#fff1f0');
+    let backgroundColor = new Color('#000000');
 
     let background = Path.Rectangle([0, 0], view.viewSize);
     background.fillColor = backgroundColor;
@@ -32,7 +33,7 @@ window.addEventListener('load', function(){
     let m = view.viewSize.width * 0.5;
     let n = view.viewSize.height * 0.5;
 
-    let count = 250;
+    let count = 270;
     let angle = Math.PI * 0.05;
     let angle2 = Math.PI * 0.05;
     let angle3 = Math.PI * 0.05;
@@ -41,39 +42,44 @@ window.addEventListener('load', function(){
     let length3 = 0;
     let dl = 0.06;
     let dl2 = 0.12;
-    let dl3 = 0.22;
-    let randomRange = 5;
+    let dl3 = 0.2;
+    let randomRange = 30;
 
     let theta = Math.PI * 0.1;
 
+    // let colors = [
+    //     '#fff1f0',
+    //     '#f7a099',
+    //     '#b84f8c',
+    //     '#9e4161',
+    // ];
+    // let colors2 = [
+    //     '#fff1f0',
+    //     '#e3d4d1',
+    //     '#5e3f5a',
+    // ];
+
     let colors = [
-        '#fff1f0',
-        '#f7a099',
-        '#b84f8c',
-        '#9e4161',
-    ];
-    let colors2 = [
-        '#fff1f0',
-        '#e3d4d1',
-        '#5e3f5a',
+        //'#68ed78'
+        '#f7ff87'
     ];
 
     let sizes = [
-        5,
+        3,
+        8,
         10,
-        15
+        20
     ];
 
-    for (let p = 0; p < view.viewSize.height; p += 20) {​​​​​​​
-        for (let o = 0; o < view.viewSize.width; o += 20) {​​​​​​​
+    for (let p = 0; p < view.viewSize.height; p += 40){
+        for (let o = 0; o < view.viewSize.width; o += 40){
             let rand_o = Math.random() * randomRange - randomRange * 0.5;
             let rand_p = Math.random() * randomRange - randomRange * 0.5;
 
-            let circle4 = Path.Circle([o + rand_o, p + rand_p], pickRandom(sizes));
-
-            circle4.fillColor = pickRandom(colors2);
-        }​​​​​​​​​​​​​​​​​​​​​​​​​​​
-    }​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​
+            let star = Path.Star([o + rand_o, p + rand_p], 5, pickRandom(sizes), pickRandom(sizes));
+            star.fillColor = pickRandom(colors);
+        }
+    }
 
     for (let i = 0; i < count * 0.6; i++) {
         m += Math.cos(theta) * length3;
@@ -83,8 +89,8 @@ window.addEventListener('load', function(){
         theta += angle3;
         length3 += dl3;
 
-        let circle3 = Path.Circle([m, n], 20);
-        circle3.fillColor = pickRandom(colors);
+        let circle = Path.Circle([m, n], 20);
+        circle.fillColor = pickRandom(colors);
 
     }
 
@@ -96,12 +102,12 @@ window.addEventListener('load', function(){
         theta += angle2;
         length2 += dl2;
 
-        let circle2 = Path.Circle([a, b], 15);
-        circle2.fillColor = pickRandom(colors);
-
+        let rect = Path.Rectangle([a, b], [20, 20]);
+        rect.fillColor = pickRandom(colors);
+        rect.rotate(45)
     }
 
-    for (let i = 0; i < count * 1.9; i++) {
+    for (let i = 0; i < count * 1.5; i++) {
         
         x += Math.cos(theta) * length;
         y += Math.sin(theta) * length;
@@ -110,7 +116,7 @@ window.addEventListener('load', function(){
         theta += angle;
         length += dl;
 
-        let circle = Path.Circle([x, y], 6);
+        let circle = Path.Circle([x, y], 3);
         circle.fillColor = pickRandom(colors);
     }
 
